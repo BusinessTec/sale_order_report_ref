c12f79d44c53f58fef03148c49a004b046602937
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-10-03 13:29:43 -0600

    Update cedula.py

--- cedula.py
+++ cedula.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
-##############################################################################
+# -*- coding: utf-8 -*- 
+
+from openerp.osv import osv,fields,orm
+
+class res_partner(osv.osv):
+
+
+	_inherit = 'res.partner'
+    	_columns = {
+    	              'cedula':fields.char('Cédula', size=20),
+    	              'tipo_de_cedula':
+    	           }
+
