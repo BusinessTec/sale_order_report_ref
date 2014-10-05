28a146bba2d462096d30c0e6140e9fd6de5e6ceb
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-10-05 11:25:02 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,6 @@
 #
 ##############################################################################
 
-import multiprice
+import cost_plus_price
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
