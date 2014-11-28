ebe6144698fb62a498be5638198475f014b1968c
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-11-27 22:05:57 -0600

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -21,13 +21,13 @@
 
 {
     'name': 'Sale Orders in Reporting Currency',
-    'version': '0.1',
+    'version': '0.2',
     'author': 'Business Tec Systems',
     'summary': 'Adds additionl column for sales order in reference currency',
     'description': """
 Sale Order in Reporting Currency
 ================================
-Calculates sales orders amount in ref currency (can be any currency or company currency). All calculation done at exchange rate of the order date.
+Calculates sales orders amount in ref currency (can be any currency) and company currency plus the same with invoices. All calculation done at exchange rate of the order date.
     """,
     'website': 'https://businesstec.net',
     'images': [],
